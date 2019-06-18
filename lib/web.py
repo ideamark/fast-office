af4ba33844268a74ed8c1cc96e8e90cb62d968b7
Mark Young <ideamark@qq.com>
2019-06-19 00:30:49 +0800

    modified:   lib/web.py

--- lib/web.py
+++ lib/web.py
@@ -8,13 +8,28 @@ class Web(object):
         self.browser.get(url)
 
     def open(self, url):
-        self.browser.get(url)
+        try:
+            self.browser.get(url)
+            return True
+        except Exception:
+            return False
 
     def click(self, xpath):
-        self.browser.find_element_by_xpath(xpath).click()
+        try:
+            self.browser.find_element_by_xpath(xpath).click()
+            return True
+        except Exception:
+            return False
 
     def read(self, xpath):
-        self.browser.find_element_by_xpath(xpath).text
+        return self.browser.find_element_by_xpath(xpath).text
 
     def write(self, xpath, text):
-        self.browser.find_element_by_xpath(xpath).send_keys(text)
+        try:
+            self.browser.find_element_by_xpath(xpath).send_keys(text)
+            return True
+        except Exception:
+            return False
+
+    def html(self):
+        return self.browser.page_source
